b0b1e6eba93b221f721d55d3b014ce67c0f76f1b
Aissatou <aissatou.onipogui@gmail.com>
2023-10-05 12:58:12 +0200

    somme de 2 nombre

--- exo3.sh
+++ exo3.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 
-#calcule de somme
+#nommer un fichier
+
 read -p "nommez le fichier: " reponse
 touch $reponse
 echo " le fichier  $reponse a été crée"
--- exo5.sh
+++ exo5.sh
@@ -1,5 +1,15 @@
 #!bin/bash
 
 #calcul de somme
-read -p 
+
+
+read -p "entrez voutre premier nombre: " nbr1
+read -p "entrez voutre second nombre: " nbr2
+sum=$(($nbr1+$nbr2))
+echo "la somme egale: "$sum
+
+
+
+
+
 
